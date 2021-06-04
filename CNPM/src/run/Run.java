20d1c4513a09cbae4065eb1f73c973a709ebd62c
Ngô Thị Tuyết Mai <nttm08032000@gmail.com>
2021-06-04 09:24:12 +0700

    Update giao dien xem chi tiet bang cham diem (Add btn Bai da cham)

--- CNPM/src/run/Run.java
+++ CNPM/src/run/Run.java
@@ -6,6 +6,8 @@ import view.ManagerCodeView;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import DTO.TableDTO;
+import DTO.UserDTO;
 import model.AnswerDAO;
 import view.AddCodeView;
 import view.DetailTableView;
@@ -16,6 +18,7 @@ public class Run {
 //		AddCodeView sign = new AddCodeView(null,null);
 //		ManagerCodeView mg = new ManagerCodeView();
 		DetailTableView detail = new DetailTableView();
+//		DetailTableView detail = new DetailTableView(new TableDTO(id, new UserDTO(id, name, email, pass, role, key, hide), tableName, numberQuestionUse, hide))
 	}
 
 }
--- CNPM/src/view/DetailTableView.java
+++ CNPM/src/view/DetailTableView.java
@@ -23,8 +23,9 @@ import controller.MarkController;
 
 public class DetailTableView extends JFrame {
 	private JButton btnBack, btnCancel, btnMinus, btnCodeManager, btnMark;
-	private TableDTO table;
+//	private TableDTO table;
 
+//	public DetailTableView(TableDTO table) {
 	public DetailTableView() {
 		getContentPane().setLayout(null);
 
@@ -79,23 +80,24 @@ public class DetailTableView extends JFrame {
 
 //		titleView
 		JLabel tittle = new JLabel("CHI TIẾT BẢNG CHẤM ĐIỂM");
+		tittle.setHorizontalAlignment(SwingConstants.CENTER);
 		tittle.setFont(new Font("Tahoma", Font.BOLD, 27));
 		tittle.setForeground(new Color(255, 255, 255));
-		tittle.setBounds(118, 0, 392, 30);
+		tittle.setBounds(0, 0, 600, 30);
 		pn1.add(tittle);
 
 //		JLabel lblTableName = new JLabel(table.getTableName());
-		JLabel lblTableName = new JLabel("Môn Toán Học"); // ví dụ
+		JLabel lblTableName = new JLabel("Demo"); // ví dụ
 		lblTableName.setForeground(Color.WHITE);
-		lblTableName.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblTableName.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblTableName.setHorizontalAlignment(SwingConstants.CENTER);
-		lblTableName.setBounds(0, 43, 600, 23);
+		lblTableName.setBounds(0, 41, 600, 23);
 		pn1.add(lblTableName);
 
 //		JLabel lblNumberQuestionUse = new JLabel("Số lượng câu hỏi: " + table.getNumberQuestionUse());
 		JLabel lblNumberQuestionUse = new JLabel("Số lượng câu hỏi: " + 10); // ví dụ
 		lblNumberQuestionUse.setForeground(Color.WHITE);
-		lblNumberQuestionUse.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNumberQuestionUse.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblNumberQuestionUse.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNumberQuestionUse.setBounds(0, 68, 600, 23);
 		pn1.add(lblNumberQuestionUse);
@@ -108,20 +110,28 @@ public class DetailTableView extends JFrame {
 		getContentPane().add(pn2);
 
 		btnCodeManager = new JButton("Quản lý mã đề");
-		btnCodeManager.setBounds(119, 25, 166, 55);
+		btnCodeManager.setBounds(55, 25, 140, 55);
 		btnCodeManager.setForeground(new Color(255, 255, 255));
-		btnCodeManager.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnCodeManager.setFont(new Font("Tahoma", Font.BOLD, 13));
 		btnCodeManager.setBackground(new Color(110, 115, 199));
 		btnCodeManager.setFocusPainted(false);
 		pn2.add(btnCodeManager);
 
+		JButton btnSeeListMark = new JButton("Bài đã chấm");
+		btnSeeListMark.setForeground(Color.WHITE);
+		btnSeeListMark.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnSeeListMark.setFocusPainted(false);
+		btnSeeListMark.setBackground(new Color(110, 115, 199));
+		btnSeeListMark.setBounds(402, 25, 140, 55);
+		pn2.add(btnSeeListMark);
+
 		btnMark = new JButton("Chấm điểm");
-		btnMark.setBounds(306, 25, 166, 55);
+		btnMark.setBounds(226, 25, 140, 55);
+		pn2.add(btnMark);
 		btnMark.setForeground(new Color(255, 255, 255));
-		btnMark.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnMark.setFont(new Font("Tahoma", Font.BOLD, 13));
 		btnMark.setBackground(new Color(110, 115, 199));
 		btnMark.setFocusPainted(false);
-		pn2.add(btnMark);
 
 //		Sử để khi chấm bài xong
 		JPanel pn3 = new JPanel();
